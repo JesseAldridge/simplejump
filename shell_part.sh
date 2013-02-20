f9e71a87887a6c06e22f73ece428b537c77023cb
Jesse Aldridge <jessealdridge@gmail.com>
2013-02-19 23:29:52 -0600

    get rid of echoes

--- shell_part.sh
+++ shell_part.sh
@@ -1,15 +1,11 @@
 
 # overwrite `cd` to write to frecency db first
 
-# echo shell part
-
 function cd { 
-    # echo overridden cd
     ~/simplejump/on_cd.py `pwd` "$@"
     builtin cd "$@"
 }
 
 function z {
-    # echo jumping to: `~/simplejump/jump.py "$@"`
     builtin cd `~/simplejump/jump.py "$@"`
 }
