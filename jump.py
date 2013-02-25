4ade4847d548128644f1cce9c6604e5fba52b730
Jesse Aldridge <jessealdridge@gmail.com>
2013-02-25 14:31:27 -0600

    remove cruft

--- jump.py
+++ jump.py
@@ -2,26 +2,19 @@
 
 import sys
 import os
-import logging
 
 import utils
 
 # Print the most frecent dir matching the query.
 
-def main():
-    # logging.info('sys.argv: {0}'.format(sys.argv))
+query = sys.argv[-1]
+if query in ['..']:
+    sys.exit()
 
-    query = sys.argv[-1]
-    if query in ['..']:
-        sys.exit()
+path_to_dir = utils.read_and_boost(query)
 
-    path_to_dir = utils.read_and_boost(query)
-
-    for dir_ in sorted(
-        path_to_dir.values(), key=lambda dir_: dir_.score, reverse=True):
-        if os.path.isdir(dir_.path):
-            print dir_.path
-            return True
-
-if __name__ == '__main__':
-    main()
+for dir_ in sorted(
+    path_to_dir.values(), key=lambda dir_: dir_.score, reverse=True):
+    if os.path.isdir(dir_.path):
+        print dir_.path
+        return True
