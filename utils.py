0361f42eb755223791689085038ead04f93d5af6
Jesse Aldridge <jessealdridge@gmail.com>
2013-02-25 19:30:02 -0600

    nicer print

--- utils.py
+++ utils.py
@@ -16,11 +16,15 @@ class Dir:
         self.timestamp = float(self.timestamp)
         self.score = frecency(self.count, self.timestamp)
 
-    def __repr__(self):
-        return '{0} {1}'.format(self.score, self.path)
-
     def serialize(self):
         return '{0} {1} {2}'.format(self.count, self.timestamp, self.path)
+
+    def print_(self):
+        print '{0:<15}| {1:<10}| count: {2}| boost: {3}| score: {4}'.format(
+            '...' + self.path[-12:], str(
+               datetime.timedelta(seconds=time.time()) - 
+               datetime.timedelta(seconds=self.timestamp)).split(',', 1)[0],
+            self.count, self.boost, int(self.score))
 
 def read_frecency_db(db_path=DB_PATH):
     ' Read frecency db.  Return mapping from paths to Dirs. '
@@ -42,20 +46,16 @@ def read_and_boost(query, db_path=DB_PATH):
 
     path_to_dir = read_frecency_db(db_path)
     for dir_ in path_to_dir.values():
-        boost(query, dir_)
+        boost = 0.
+        if query in os.path.basename(dir_.path):
+            boost = 3.
+        elif query.lower() in os.path.basename(dir_.path).lower():
+            boost = 2.
+        elif query in dir_.path:
+            boost = 1.
+        dir_.boost = boost
+        dir_.score += dir_.count * boost
     return path_to_dir
-            
-
-def boost(query, dir_):
-    boost = 0
-    if query in os.path.basename(dir_.path):
-        boost = 3.
-    elif query.lower() in os.path.basename(dir_.path).lower():
-        boost = 2.
-    elif query in dir_.path:
-        boost = 1.
-    dir_.boost = boost
-    dir_.score += dir_.count * boost
 
 def frecency(count, timestamp):
     ' Take into account frequency and recency. '
@@ -75,16 +75,11 @@ if __name__ == '__main__':
         dirs = sorted(path_to_dir.values(), 
                       key=lambda d: d.score, reverse=True)
         actual_order = [os.path.basename(d.path) for d in dirs]
-        print 'desired:', desired_order
-        print
         print 'query:', query
-        print 'actual:', actual_order
-        print 'timedeltas:', [
-            str(datetime.timedelta(seconds=time.time()) - 
-                datetime.timedelta(seconds=dir_.timestamp)).split(',', 1)[0]
-            for dir_ in dirs]
-        print 'counts:', [dir_.count for dir_ in dirs]
-        print 'boosts:', [dir_.boost for dir_ in dirs]
-        print 'scores:', [int(dir_.score) for dir_ in dirs]
+        print 'desired:', desired_order
+        print 'actual:'
+        for dir_ in dirs:
+            dir_.print_()
+        print
         sys.stdout.flush()
         assert desired_order == actual_order
