cf3eeee6480b0fef1e40ad1d8fe1ff0b5c60a81b
Jesse Aldridge <jessealdridge@gmail.com>
2015-02-09 12:28:25 -0800

    clarify stole message

--- jump.py
+++ jump.py
@@ -5,7 +5,7 @@ import sys, os, subprocess
 import utils
 
 if 'SIMPLE_JUMP_CD' not in os.environ:
-    sys.stderr.write('Something stole cd!\n')
+    sys.stderr.write('simplejump: Something stole cd!\n')
     proc = subprocess.Popen(['type', 'cd'], stdout=subprocess.PIPE)
     sys.stderr.write(proc.stdout.read())
 
